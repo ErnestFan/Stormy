20ab04c63a899d7669ee718a4a56e01900f9e256
ErnestFan <ernestfan1019@gmail.com>
2017-06-28 23:26:49 -0700

    Initial Commit

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Ernest Fan on 2017-06-27.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipitationProbability: Double
+    let summary: String
+    let icon: String
+}
